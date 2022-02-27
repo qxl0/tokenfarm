37d763e715862f9df3edd7843d08ee22a43a601a
qiang li <qxl0@yahoo.com>
2022-02-26 19:56:31 -0500

    Cleaned up unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { ChainId, DAppProvider } from "@usedapp/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello, qiang</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+      }}
+    >
+      <div className="App">Hi</div>
+    </DAppProvider>
   );
 }
 
