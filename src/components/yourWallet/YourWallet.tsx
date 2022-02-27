07fe9966f80aa59e8a04901b86fd71efb8083476
qiang li <qxl0@yahoo.com>
2022-02-27 11:08:59 -0500

    Tab change works

--- src/components/yourWallet/YourWallet.tsx
+++ src/components/yourWallet/YourWallet.tsx
@@ -1,6 +1,6 @@
 import { Box, Tab } from "@material-ui/core";
 import { TabContext, TabList } from "@material-ui/lab";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Token } from "../Main";
 
 interface YourWalletProps {
@@ -8,12 +8,15 @@ interface YourWalletProps {
 }
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
   const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0);
+  const handleChange = (e: React.ChangeEvent<{}>, newValue: string) => {
+    setSelectedTokenIndex(parseInt(newValue));
+  };
   return (
     <Box>
       <h1>Your wallet</h1>
       <Box>
         <TabContext value={selectedTokenIndex.toString()}>
-          <TabList aria-label="stake form tabs">
+          <TabList onChange={handleChange} aria-label="stake form tabs">
             {supportedTokens.map((token, index) => {
               return (
                 <Tab
