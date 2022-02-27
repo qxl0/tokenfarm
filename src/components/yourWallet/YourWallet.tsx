cb177286f2b3bb2fbcdfe4a297a6d580bef2d776
qiang li <qxl0@yahoo.com>
2022-02-27 11:07:04 -0500

    Added tablist in Wallet

--- src/components/yourWallet/YourWallet.tsx
+++ src/components/yourWallet/YourWallet.tsx
@@ -1,8 +1,31 @@
+import { Box, Tab } from "@material-ui/core";
+import { TabContext, TabList } from "@material-ui/lab";
+import { useState } from "react";
 import { Token } from "../Main";
 
 interface YourWalletProps {
   supportedTokens: Array<Token>;
 }
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
-  return <div>Your Wallet</div>;
+  const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0);
+  return (
+    <Box>
+      <h1>Your wallet</h1>
+      <Box>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList aria-label="stake form tabs">
+            {supportedTokens.map((token, index) => {
+              return (
+                <Tab
+                  label={token.name}
+                  value={index.toString()}
+                  key={index}
+                ></Tab>
+              );
+            })}
+          </TabList>
+        </TabContext>
+      </Box>
+    </Box>
+  );
 };
