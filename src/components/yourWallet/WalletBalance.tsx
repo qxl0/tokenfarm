0ab0a5e957c29f02bfaca270a9b68e37b31842d3
qiang li <qxl0@yahoo.com>
2022-02-27 11:24:50 -0500

    Added etherproject/units

--- src/components/yourWallet/WalletBalance.tsx
+++ src/components/yourWallet/WalletBalance.tsx
@@ -1,3 +1,4 @@
+import { formatUnits } from "@ethersproject/units";
 import { useEthers, useTokenBalance } from "@usedapp/core";
 import React from "react";
 import { Token } from "../Main";
@@ -9,12 +10,15 @@ export default function WalletBalance({ token }: WalletBalanceProps) {
   const { image, address, name } = token;
   const { account } = useEthers();
   const tokenBalance = useTokenBalance(address, account);
-  console.log(tokenBalance);
+  const formattedTokenBalance: number = tokenBalance
+    ? parseFloat(formatUnits(tokenBalance))
+    : 0;
 
   return (
     <div>
       <div>{name}</div>
       <div>{address}</div>
+      <div>{formattedTokenBalance}</div>
     </div>
   );
 }
